5cf983ae419754f142daf26c784d38fdcb76ca13
Mikiyas Alehegn <mikiyas.alehegn@linearb.io>
2025-04-01 23:18:35 +0300

    db connection updated

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const connectionString = process.env.DATABASE_URL;
 const client = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: {
     rejectUnauthorized: process.env.SSL_REJECT_UNAUTHORIZED === "true",
   },
